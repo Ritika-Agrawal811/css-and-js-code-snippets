3f191a09432e1df588b7f126ef4af84bf1483472
Ritika Agrawal <ritikaagrawal339@gmail.com>
2024-01-15 17:25:38 +0530

    refactor: add all nextjs files to website folder

---
+++ website/src/app/(site)/page.tsx
@@ -0,0 +1,25 @@
+
+
+const categories = ['All', 'Buttons', 'Loaders', 'Icons', 'Carousels', 'Form Fields', 'Hover Effects', 'Text Animations', 'Miscellaneous']
+
+export default function Home() {
+  return (
+    <>
+      <header className="flex flex-col items-center justify-center min-h-screen bg-red-100">
+      <h1 className="my-8 text-5xl">CSS and JS Code Snippets</h1>
+      <p>Find real-world code snippets to use in your projects!</p>
+      </header>
+      <main className="w-4/5 mx-auto my-8">
+        <section className="space-y-6">
+          <div className="flex justify-between">
+          <h2 className="text-3xl">Snippets</h2>
+           <button>Search</button>
+          </div>
+          <ul className="flex justify-between">
+            {categories.map(category =>  <li key={category} className="border-2 border-purple-700 rounded-full px-4 py-0.5 text-purple-700 font-medium cursor-pointer">{category}</li>)}                   
+          </ul>
+        </section>
+      </main>
+    </>
+  );
+}
