ec2376e363ffdf4a7ca7ee49bdb699379e2f19d2
Ritika-Agrawal811 <Ritika-Agrawal811@users.noreply.github.com>
2024-02-15 18:14:13 +0000

    style: format files

--- Text Animations/Text_Rotate_Animation/script.js
+++ Text Animations/Text_Rotate_Animation/script.js
@@ -1,17 +1,16 @@
-const rotateText = document.querySelector('.rotate-text')
-const headings = ['Hey', 'Hola', 'Yo', 'Hello'];
+const rotateText = document.querySelector(".rotate-text");
+const headings = ["Hey", "Hola", "Yo", "Hello"];
 let index = 0;
 
-
 function changeRotateText() {
-    rotateText.classList.remove('flip')
-    
-    setTimeout(() => {
-        rotateText.innerHTML = headings[index];
-        rotateText.classList.add('flip')
-    }, 850)
-    
-    index = (index + 1) % headings.length;
+  rotateText.classList.remove("flip");
+
+  setTimeout(() => {
+    rotateText.innerHTML = headings[index];
+    rotateText.classList.add("flip");
+  }, 850);
+
+  index = (index + 1) % headings.length;
 }
 
-setInterval(changeRotateText, 1700)
+setInterval(changeRotateText, 1700);
