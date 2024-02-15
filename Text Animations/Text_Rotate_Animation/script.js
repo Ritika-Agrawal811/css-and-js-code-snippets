89f167772213eea28161fc2ce36ea0d552dbf769
Ritika Agrawal <ritikaagrawal339@gmail.com>
2024-02-15 23:40:53 +0530

    feat: add text rotate animation

---
+++ Text Animations/Text_Rotate_Animation/script.js
@@ -0,0 +1,17 @@
+const rotateText = document.querySelector('.rotate-text')
+const headings = ['Hey', 'Hola', 'Yo', 'Hello'];
+let index = 0;
+
+
+function changeRotateText() {
+    rotateText.classList.remove('flip')
+    
+    setTimeout(() => {
+        rotateText.innerHTML = headings[index];
+        rotateText.classList.add('flip')
+    }, 850)
+    
+    index = (index + 1) % headings.length;
+}
+
+setInterval(changeRotateText, 1700)
