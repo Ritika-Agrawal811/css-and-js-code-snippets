3e38c4714bdfb8bd4a58bac9baf59c31edf3a9ac
Ritika-Agrawal811 <Ritika-Agrawal811@users.noreply.github.com>
2024-03-05 09:21:36 +0000

    style: format files

--- Miscellaneous/Navigation_Menu_Animation/script.js
+++ Miscellaneous/Navigation_Menu_Animation/script.js
@@ -1,6 +1,6 @@
-const link = document.querySelector('.link');
-const header = document.getElementById('header')
+const link = document.querySelector(".link");
+const header = document.getElementById("header");
 
-link.addEventListener('click', () => { 
-  header.classList.toggle('show');
-})
+link.addEventListener("click", () => {
+  header.classList.toggle("show");
+});
