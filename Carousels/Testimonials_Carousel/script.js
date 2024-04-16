64567530b481cfe89e4e3da7c033ff1c5e44d5f0
Ritika Agrawal <ritikaagrawal339@gmail.com>
2024-04-16 15:53:52 +0530

    chore: format the files

---
+++ Carousels/Testimonials_Carousel/script.js
@@ -0,0 +1,34 @@
+[
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nullamolestiae repudiandae harum eveniet corrupti eaque? Amet numquam doloremincidunt.",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nullamolestiae repudiandae harum eveniet corrupti eaque? Amet numquam doloremincidunt.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nullamolestiae repudiandae harum eveniet corrupti eaque? Amet numquam doloremincidunt.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nullamolestiae repudiandae harum eveniet corrupti eaque? Amet numquam doloremincidunt.",
+  },
+];
